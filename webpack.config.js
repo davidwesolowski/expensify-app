3edd5b4fd061f6108116b2e344c17d763a805843
Dawid Wesołowski <s169122@student.pg.edu.pl>
2019-11-02 17:16:56 +0100

    Added webpack production stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,46 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports =
+module.exports = (env) =>
 {
-    entry: './src/app.js',
-    output:
-    {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: "development",
-    module:
-    {
-        rules:[{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },
+    const isProduction = env === 'production';
+    const MiniCSSExtract = new MiniCssExtractPlugin({ filename: 'style.css' });
+    return {
+        entry: './src/app.js',
+        output:
         {
-            use:[ 'style-loader', 'css-loader', 'sass-loader'],
-            test: /\.s?css$/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer:
-    {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        mode: isProduction ? "production" : "development",
+        module:
+        {
+            rules:[{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use:
+                [ 
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options:
+                        {
+                            hmr: process.env.NODE_ENV === 'development'
+                        }
+                    }, 
+                    'css-loader', 'sass-loader'
+                ],
+                test: /\.s?css$/
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer:
+        {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        plugins: [MiniCSSExtract]
+    }; 
 };
